722ce2793dc13965b0f0a7e082b3775ee0c3e1f1
hongduccodedao <contact.hongduc@gmail.com>
2023-11-13 22:44:25 +0700

    Header

---
+++ components/Header/ToggleExpand.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useStore } from "@/stores";
+import { Maximize2, Minimize2 } from "lucide-react";
+import React, { useEffect } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+import { closeFullscreen, openFullscreen } from "@/utils/fullscreen";
+import useWindowStore from "@/stores/expandWindow";
+
+const ToggleExpand = () => {
+  const { isExpanded, toggleExpand } = useWindowStore();
+
+  const handleToggleExpand = () => {
+    toggleExpand();
+
+    if (isExpanded) {
+      closeFullscreen();
+    } else {
+      openFullscreen();
+    }
+  }; 
+
+  return (
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <div onClick={handleToggleExpand}>
+              {isExpanded ? (
+                <Minimize2 size={25} color="white" />
+              ) : (
+                <Maximize2 size={25} color="white" />
+              )}
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            {isExpanded ? "Minimize the window" : "Expand the window"}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
+  );
+};
+
+export default ToggleExpand;
